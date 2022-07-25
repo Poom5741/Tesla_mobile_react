d5281ec94f05c6dea3c819b7b6e4164db398f09e
poom <poom..@Pooms-MacBook-Pro.local>
2022-07-25 23:01:27 +0700

    Button Component

--- component/CarItem/index.js
+++ component/CarItem/index.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { View, Text, ImageBackground } from "react-native";
+import StyledButton from "../StyledButton";
 import styles from "./styles";
 
 function CarItem() {
   return (
     <View style={styles.carContainer}>
       <ImageBackground
-        source={require("../../assets/assets/Images/ModelX.jpeg")}
+        source={require("./../../assets/assets/images/ModelX.jpeg")}
         style={styles.image}
       />
       <View style={styles.titles}>
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
+
+      <StyledButton
+        type="primary"
+        content={"Custom Order"}
+        onPress={() => {
+          console.warn("Custom Order was pressed");
+        }}
+      />
+
+      <StyledButton
+        type="secondary"
+        content={"Existing Inventory"}
+        onPress={() => {
+          console.warn("Existing Inventory was pressed");
+        }}
+      />
     </View>
   );
 }
